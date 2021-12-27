c274b8841443f217505aa4fe44626965fa19afae
“sagarrajput91” <sagar.rajput91@gmail.com>
2021-12-27 11:46:27 +0530

    added runner.

---
+++ src/test/java/test1.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class test1 {
+
+    @Test
+    public void testMethod(){
+        Assert.assertTrue(1==1);
+    }
+}
